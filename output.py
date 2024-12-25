4b3c66487be53b34e0b41b8b8806294a6b3a4d96
Sainithin139 <151899339+Sainithin139@users.noreply.github.com>
2024-12-25 18:04:39 +0530

    Create output.py

---
+++ output.py
@@ -0,0 +1,10 @@
+Student A1 authenticated successfully.
+Professor P1 authenticated successfully.
+Professor P1 set availability: ['T1', 'T2', 'T3']
+Available slots for P1: ['T1', 'T2', 'T3']
+Student A1 booked appointment with Professor P1 at T1.
+Student A2 authenticated successfully.
+Now Available slots for P1: ['T2', 'T3']
+Student A2 booked appointment with Professor P1 at T2.
+Professor P1 canceled appointment with Student A1 at T1.
+Appointments for A1: []
